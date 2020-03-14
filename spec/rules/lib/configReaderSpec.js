cd503c22d6eeea3e892ba1be6f5d3c68958bedab
Philip Skinner <me@philip-skinner.co.uk>
2020-03-14 21:23:29 +0000

    [+] Adds some specs to confirm test coverage works

--- spec/rules/lib/configReaderSpec.js
+++ spec/rules/lib/configReaderSpec.js
@@ -3,8 +3,83 @@ const
 	sinon 			= require('sinon'),
 	configReader 	= require('../../../rules/lib/configReader');
 
-describe("A config reader", () => {
-	it("empty test", () => {
+const fs = {
+	readFile : () => {}
+};
 
+const path = {
+	join : () => {}
+};
+
+const constructorTest = (done) => {
+	const instance = configReader();
+
+	expect(instance.fs).not.toBe(null);
+	expect(instance.path).not.toBe(null);
+
+	done();
+};
+
+const fileReadError = (done) => {
+	const fsMock = sinon.mock(fs);
+	fsMock.expects('readFile').once().withArgs('what.json').callsArgWith(1, new Error('oops'));
+
+	const pathMock = sinon.mock(path);
+	pathMock.expects('join').once().withArgs(process.cwd(), './doot.json').returns('what.json');
+
+	const instance = configReader(fs, path);
+	instance.readDefinition("./doot.json").catch((err) => {
+		expect(err).toEqual(new Error('oops'));		
+
+		fsMock.verify();
+		pathMock.verify();
+
+		done();
+	});
+};
+
+const invalidJSON = (done) => {
+	const fsMock = sinon.mock(fs);
+	fsMock.expects('readFile').once().withArgs('what.json').callsArgWith(1, null, "lijef}{}{}{}{");
+
+	const pathMock = sinon.mock(path);
+	pathMock.expects('join').once().withArgs(process.cwd(), './doot.json').returns('what.json');
+
+	const instance = configReader(fs, path);
+	instance.readDefinition("./doot.json").catch((err) => {
+		expect(err).toEqual(new Error('Cannot read rules definition ./doot.json'));		
+
+		fsMock.verify();
+		pathMock.verify();
+
+		done();
+	});
+};
+
+const success = (done) => {
+	const fsMock = sinon.mock(fs);
+	fsMock.expects('readFile').once().withArgs('what.json').callsArgWith(1, null, '{"hello":"world"}');
+
+	const pathMock = sinon.mock(path);
+	pathMock.expects('join').once().withArgs(process.cwd(), './doot.json').returns('what.json');
+
+	const instance = configReader(fs, path);
+	instance.readDefinition("./doot.json").then((config) => {
+		expect(config.hello).toEqual("world");
+
+		fsMock.verify();
+		pathMock.verify();
+
+		done();
+	});
+};
+
+describe("A config reader", () => {
+	it("defaults deps in its constructor", constructorTest);
+
+	describe("can read rule configuration", () => {
+		it("handling file read errors", fileReadError);
+		it("handling invalid JSON", invalidJSON);
+		it("successfully", success);
 	});
 });
