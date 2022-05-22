804f0fda066820f9e68d1d44f6171f6d369761dd
Philip Skinner <me@philip-skinner.co.uk>
2022-05-22 12:14:35 +0100

    [+] Adds rate limiting onto API service

--- src/service.api/main.js
+++ src/service.api/main.js
@@ -2,13 +2,18 @@ const
     express 		= require('express'),
     path 			= require('path'),
     tokenHandler 	= require('../support.lib/tokenHandler'),
-    hotreload 		= require('../support.lib/hotReload')();
-bodyParser 		= require('body-parser');
+    hotreload 		= require('../support.lib/hotReload')(),
+    bodyParser 		= require('body-parser'),
+    rateLimit       = require('express-rate-limit');
 
 let app = null;
 let server = null;
 let restarting = false;
-let tHandler = tokenHandler();
+const tHandler = tokenHandler();
+const limiter = rateLimit({
+    windowMs: process.env.RATE_LIMIT_WINDOW_MILLISECONDS  || 200,
+    max: process.env.RATE_LIMIT_MAX_REQUESTS_IN_WINDOW    || 50
+});
 
 if (!process.env.DIR) {
     throw new Error('Require dir to load configuration from.');
@@ -19,13 +24,14 @@ if (!process.env.PORT) {
 }
 
 const startup = () => {
-    app = express();
+    app = express();    
 
-    const apiService = require('./lib/apiService')(app);
-
+    app.use(limiter);
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended : false }));
     app.use(tHandler.tokenCheck.bind(tHandler));
+
+    const apiService = require('./lib/apiService')(app);
 
     //load our APIs
     apiService.init(process.env.DIR).then(() => {
